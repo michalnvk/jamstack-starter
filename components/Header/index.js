df9a2a2f2eca4b7b3ed56543a226bbcc75158768
Michal Novak <hi@michal.works>
2021-12-17 00:23:05 +0100

    Basic styled components

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,27 +1,38 @@
 import Link from "next/link";
+import styled from "styled-components";
 import { useI18n } from "../../contexts/i18nContext";
 
 export default function Header() {
   const i18n = useI18n();
 
   return (
-    <>
-      <header>
-        LOGO
-        
-        <nav>
-          <ul>
-            {i18n.topmenu.map((menuItem, index) => (
-              <li key={index}> 
-                <Link href={menuItem.url}>
-                  <a>{menuItem.name}</a>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
+    <StyledHeader>
+      <span>Logo</span>
 
-      </header>
-    </>
+      <nav>
+        <ul>
+          {i18n.topmenu.map((menuItem, index) => (
+            <li key={index}>
+              <Link href={menuItem.url}>
+                <a>{menuItem.name}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </StyledHeader>
   );
 }
+
+const StyledHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  ul {
+    display: flex;
+    list-style: none;
+    li {
+      margin: 5px;
+    }
+  }
+`;
