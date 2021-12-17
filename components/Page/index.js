67bd7ce39ee464950ac1a5c6cec9fefcfdfb5ab5
Michal Novak <hi@michal.works>
2021-12-18 00:00:51 +0100

    global styles

--- components/Page/index.js
+++ components/Page/index.js
@@ -1,10 +1,12 @@
 import { MDXRemote } from "next-mdx-remote";
 import dynamic from "next/dynamic";
 
-import Header from "../Header";
 import Footer from "../Footer";
+import Header from "../Header";
 import LayoutDefault from "../LayoutDefault";
 import { NextSeo } from "next-seo";
+
+import { createGlobalStyle } from "styled-components";
 
 import FrontMatterProvider from "../../contexts/frontMatterContext";
 import I18nProvider from "../../contexts/i18nContext";
@@ -61,6 +63,7 @@ export default function Page({
       />
       <I18nProvider i18n={i18n}>
         <Header />
+        <GlobalStyle />
         <FrontMatterProvider frontMatter={frontMatter}>
           <ContentLayout currentUrl={currentUrl}>
             {children ? (
@@ -75,3 +78,75 @@ export default function Page({
     </>
   );
 }
+
+// Project global styles
+const GlobalStyle = createGlobalStyle`
+  :root {
+    --font-primary: 'Noto Sans Display', sans-serif;
+    --font-secondary: 'Poppins', sans-serif;
+
+    --font-size-sm: 13px;
+    --font-size-md: 16px;
+    --font-size-lg: 22px;
+    --font-size-xl: 32px;
+    --font-size-xxl: 45px;
+
+    --line-height-text: 150%;
+    --line-height-headlines: 90%;
+
+    --font-weight-normal: 300;
+    --font-weight-semi: 500;
+    --font-weight-bold: 700;
+
+    --color-primary: #ccc;
+    --color-primary-highlight: #aaa;
+    --color-primary-light: #ddd;
+    --color-text: #444;
+    --color-border: #444;
+
+    --transition: .3s;
+    --border-radius: 3px;
+
+    --space-xs: 5px;
+    --space-sm: 10px;
+    --space-md: 20px;
+    --space-lg: 40px;
+    --space-xl: 80px;
+    --space-xxl: 120px;
+
+    --content-max-width: 1400px;
+    --content-narrow-width: 700px;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }  
+  
+  body {
+    font-family: var(--font-primary);
+    color: var(--color-text);
+    line-height: var(--line-height-text);
+    font-weight: var(--font-weight-normal)
+  }
+  
+  h1, h2, h3, h4, p, ol, ul {
+    margin-bottom: var(--space-md);
+  }
+
+  h1, h2, h3, h4 {
+    font-family: var(--font-secondary);  
+  }
+
+  h1 {
+    font-size: var(--font-size-xxl);
+  }
+  h2 {
+    font-size: var(--font-size-xl);
+  }
+  h3 {
+    font-size: var(--font-size-lg);
+  }
+
+`;
--- pages/_app.js
+++ pages/_app.js
@@ -11,9 +11,16 @@ const MyApp = ({ Component, pageProps }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
         <link rel="shortcut icon" href="/favicon.ico" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:wght@300;500;700&family=Poppins:wght@300;500;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Component {...pageProps} />
     </>
   );
-}
+};
 export default MyApp;
